5b3579cd9f5dd20f35c1211d82491e76c38bf7c6
rizky herdiansyah <rizkyherdiansyah31@gmail.com>
2024-06-12 21:22:03 +0700

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -12,31 +12,11 @@ const Login = () => {
                 />
             </div>
             <div className="md:w-1/3 max-w-sm">
-                <div className="text-center md:text-left">
-                    <label className="mr-1">Sign in with</label>
-                    {/* <button
-                        type="button"
-                        className="mx-1 h-9 w-9  rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]"
-                    >
-                        <BiLogoFacebook
-                            size={20}
-                            className="flex justify-center items-center w-full"
-                        />
-                    </button>
-                    <button
-                        type="button"
-                        className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]"
-                    >
-                        <AiOutlineTwitter
-                            size={20}
-                            className="flex justify-center items-center w-full"
-                        />
-                    </button> */}
+                <div className="text-center md:text-center">
+                    <label className="mr-1">Sign in</label>
                 </div>
                 <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-                    <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
-                        Or
-                    </p>
+                    
                 </div>
                 <input
                     className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
@@ -60,7 +40,7 @@ const Login = () => {
                         Forgot Password?
                     </a>
                 </div>
-                <div className="text-center md:text-left">
+                <div className="text-center md:text-center">
                     <button
                         className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
                         type="submit"
