d1fd2ef473c242770dfadbafe6099ed420b84420
rizky herdiansyah <rizkyherdiansyah31@gmail.com>
2024-06-12 21:16:42 +0700

    init init

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { AiOutlineTwitter } from "react-icons/ai";
-import { BiLogoFacebook } from "react-icons/bi";
+// import React from "react";
+// import { AiOutlineTwitter } from "react-icons/ai";
+// import { BiLogoFacebook } from "react-icons/bi";
 
 const Login = () => {
     return (
@@ -14,7 +14,7 @@ const Login = () => {
             <div className="md:w-1/3 max-w-sm">
                 <div className="text-center md:text-left">
                     <label className="mr-1">Sign in with</label>
-                    <button
+                    {/* <button
                         type="button"
                         className="mx-1 h-9 w-9  rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]"
                     >
@@ -31,7 +31,7 @@ const Login = () => {
                             size={20}
                             className="flex justify-center items-center w-full"
                         />
-                    </button>
+                    </button> */}
                 </div>
                 <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
                     <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
