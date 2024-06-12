3eba6137114979ec87bd23bc51e16a14504c3b09
Dysan2203 <hadykw1@gmail.com>
2024-06-12 22:47:07 +0700

    yu

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -4,20 +4,21 @@
 
 const Login = () => {
     return (
-        <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-            <div className="md:w-1/3 max-w-sm">
+        <div className="h-screen flex flex-col md:flex-row space-y-10 md:space-y-md:space-x-160  items-center my-2 mx-5 md:mx-0 md:my-0">
+            <div className="h-full bg-[#29ADB2]">
                 <img
-                    src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    src="assets/images/login.png"
                     alt="Sample image"
                 />
             </div>
+
             <div className="md:w-1/3 max-w-sm">
                 <div className="text-center md:text-center">
-                    <label className="mr-1">Sign in</label>
+                    <label className="mr-1 ">FIQIH <span className="text-[#29ADB2]">KELUARGA</span></label>
                 </div>
-                <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-                    
-                </div>
+                {/* <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+
+                </div> */}
                 <input
                     className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
                     type="text"
@@ -34,7 +35,7 @@ const Login = () => {
                         <span>Remember Me</span>
                     </label>
                     <a
-                        className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
+                        className=" hover:text-blue-700 hover:underline hover:underline-offset-4"
                         href="#"
                     >
                         Forgot Password?
@@ -42,7 +43,7 @@ const Login = () => {
                 </div>
                 <div className="text-center md:text-center">
                     <button
-                        className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+                        className="mt-4 bg-blue-600 hover:bg-blue-700 px-10 py-2 text-white uppercase rounded text-xs tracking-wider"
                         type="submit"
                     >
                         Login
@@ -58,7 +59,7 @@ const Login = () => {
                     </a>
                 </div>
             </div>
-        </section>
+        </div>
     );
 };
 
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Register = () => {
+    return (
+        <div className='w-full h-screen bg-black'>
+            <h1 className='text-white'>TEST</h1>
+        </div>
+    )
+}
