6fc494ede3602f048b2d1b6a404b9fffecfad032
Dysan2203 <hadykw1@gmail.com>
2024-06-13 14:11:12 +0700

    login done

---
+++ Frontend/src/css/login.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHref = styled.a`
+decoration : underline;
+color : blue;
+cursor : pointer;
+
+&:hover {
+color : #0766AD;
+}
+`
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyledHref } from "../css/login";
 // import { AiOutlineTwitter } from "react-icons/ai";
 // import { BiLogoFacebook } from "react-icons/bi";
 
@@ -6,14 +7,13 @@ const Login = () => {
     return (
         <div className="flex h-screen">
             {/* Colored half */}
-            <div className="hidden lg:flex lg:w-1/2 bg-[#29ADB2] items-center justify-center">
-                <img src="assets/images/login.png" alt="Placeholder" className=" w-1/2 h-1/2" />
+            <div className="hidden lg:flex lg:w-1/2 bg-[#29ADB2] items-center justify-center pb-20">
+                <img src="assets/images/login.png" alt="Placeholder" className=" w-90 h-80 " />
             </div>
-
             {/* Non-colored half (Login form) */}
             <div className="flex w-full lg:w-1/2 items-center justify-center">
                 <div className="max-w-md w-full p-8">
-                    <h2 className="text-3xl font-bold text-gray-700 text-center">FIQIH <span className="text-[#29ADB2]">KELUARGA</span></h2>
+                    <h2 className="text-3xl font-bold text-gray-700 text-center pb-20">FIQIH <span className="text-[#29ADB2]">KELUARGA</span></h2>
                     <form className="mt-8 space-y-6">
                         <div>
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
@@ -37,19 +37,22 @@ const Login = () => {
                                 placeholder="**********"
                             />
                         </div>
-                        <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-center">
                             <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                className="bg-[#0766AD] hover:bg-blue-700 text-white font-bold py-2 px-20 rounded focus:outline-none focus:shadow-outline text-center"
                                 type="button"
                             >
                                 Sign In
                             </button>
-                            <a
+                            {/* <a
                                 className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                                 href="#"
                             >
                                 Forgot Password?
-                            </a>
+                            </a> */}
+                        </div>
+                        <div className="text-center">
+                            Belum punya akun? <StyledHref>Daftar!</StyledHref>
                         </div>
                     </form>
                 </div>
