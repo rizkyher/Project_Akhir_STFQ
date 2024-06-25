9fae83bff29e3367b21d2b19fd6211bddbaca4b7
Dysan2203 <hadykw1@gmail.com>
2024-06-25 21:31:30 +0700

    nyobi

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -111,11 +111,11 @@ export function Footer() {
 
             {/* <!--Copyright section--> */}
             <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
-                <span>© 2023 Copyright:</span>
+                <span>© 2023 Copyright:  </span>
                 <a
                     className="font-semibold text-neutral-600 dark:text-neutral-400"
                     href="https://tw-elements.com/"
-                >TW Elements</a>
+                >STFQ PLUS BANDUNG</a>
             </div>
         </footer>
     );
